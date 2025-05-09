0a3a9bcb79f24f554871066e531f6dfb525f5da1
arryyyXD <arina50346@gmail.com>
2025-05-10 02:39:16 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -5,8 +9,8 @@ export default defineConfig({
       '/api': {
         target: 'https://app-planer.online',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path: string) => path.replace(/^\/api/, ''),
       },
     },
-  }
-})
+  },
+});
